4d897b577d593d89c527dd7c171ae7d1172a6915
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-03-10 12:00:42 +0000

    add image section

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('.cta-buttons')
     ];
 
+    // 슬라이더에 대한 애니메이션 
+    const sliderContainer = document.querySelector('.slider-container');
+    let currentIndex = 0;
+    const slideCount = document.querySelectorAll('.slider-item').length;
+
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % slideCount;
+        sliderContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }, 3000); 
+
+
     setTimeout(() => {
         elements.forEach((element, index) => {
             setTimeout(() => {
